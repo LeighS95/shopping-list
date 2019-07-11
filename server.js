6740bce385eb3980cd205180c27a876ae4629af6
Stephen Leigh <leigh.s95@outlook.com>
2019-07-11 12:13:46 +0100

    update

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 //Serve Static assets (build) if in production
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     //set static folder for true
     app.use(express.static('client/build'));
     //Loading html
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    })
+    });
 }
 
 const port = process.env.PORT || 5000;
