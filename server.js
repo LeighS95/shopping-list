162c5bf34435d701a459e520b2565a76e42aefb6
Stephen Leigh <leigh.s95@outlook.com>
2019-07-13 02:27:55 +0100

    upd

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
-const config = require('config');
+const config = require('./config/default');
 
 const app = express();
 
